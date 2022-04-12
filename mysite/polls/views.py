f8da68c0f89dc5a3f76c9e459568e0557a6356ec
Gregory Brivady <gregory.brivady@student.isae-supaero.fr>
2022-04-12 11:14:44 +0200

    only display the detail of a question if it is published
    Prevents an user from using the question id to acces the detail page of the question, by querying the right url

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -19,6 +19,10 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        #Exclude any questions that aren't publised yet
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
 
 
 class ResultsView(generic.DetailView):
